a55ae15e1c58e08ae05a2c47c3fb4535219d5c11
Dusan Mijatovic (myLinux 2018) <d.mijatovic@dv4all.nl>
2018-06-11 09:03:53 +0200

    resposive part 1

--- src/component/header.vue
+++ src/component/header.vue
@@ -1,12 +1,39 @@
 <template>
   <header class="header">
-    Header
+    <img :src="logo.src" :alt="logo.label" class="header-logo">
+    <h3 class="h-3">{{ title }}</h3>
+    <h1 class="h-1">{{ callout }}</h1>
+    <button class="btn header-button">View our properties</button>
+    <div class="header-seenon-text">
+      {{seenon.title}}
+    </div>
+    <div class="header-seenon-logos">
+      <img v-for="logo in seenon.logos" 
+        :src="logo.src" alt=""
+        :key="logo.src">
+    </div>
   </header>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      logo:{src:'img/logo.png', label:'Nexter logo'},
+      title:'Your own home:',
+      callout:'Ultimate personal freedom',
+      seenon:{
+        title:'As seen on',
+        logos:[
+          {src:'img/logo-bbc.png', label:'BBC logo'},
+          {src:'img/logo-forbes.png', label:'Forbes logo'},
+          {src:'img/logo-techcrunch.png', label:'Techcrunch logo'},
+          {src:'img/logo-bi.png', label:'BI logo'},
+        ]
+      }
 
+    }
+  }
 }
 </script>
 
--- src/component/realtors.vue
+++ src/component/realtors.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="realtors">
-    Realtors
+    <h3 class="h-3">{{title}}</h3>
+    <div class="realtors-list">
+      <template v-for="item in realtors">
+        <img :src="item.img.src" 
+          :alt="item.img.label" 
+          :key="item.img.src"
+          class="realtors-img">
+
+        <div class="realtors-details"
+          :key="item.img.name">
+          <h4 class="h-4 light">{{item.name}}</h4>
+          <p class="realtors-sold">{{item.sold}}</p>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      title:'Top 3 relators',
+      realtors:[{
+        name:'Erik Feinman', sold:'245 houses sold',
+          img:{src:'img/realtor-1.jpeg', label:"Photo Erik Feinman"}
+      },{
+        name:'Kim Brown', sold:'215 houses sold',
+          img:{src:'img/realtor-2.jpeg', label:"Photo Erik Feinman"}
+      },{
+        name:'Toby Ramey', sold:'198 houses sold',
+          img:{src:'img/realtor-3.jpeg', label:"Photo Erik Feinman"}
+      }]
+    }
+  }
 }
 </script>
 
